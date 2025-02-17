b84e69c7d5c35381c6d1531dda8d87ad846557fc
lukkaz2709 <lmiguez2709@gmail.com>
2025-02-17 10:30:17 -0300

    refinando meu projeto de Conta-Banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -8,25 +8,22 @@ public class ContaTerminal {
 
         System.out.println("Digite o número da sua conta: ");
         int numero = scanner.nextInt();
+        scanner.nextLine(); // Consome a quebra de linha pendente
 
         System.out.println("Agora digite o número da sua agência: ");
-        String agencia = scanner.next();
+        String agencia = scanner.nextLine();
 
         System.out.println("Por favor, digite seu nome completo: ");
-        String nomeCliente = scanner.next();
+        String nomeCliente = scanner.nextLine();
 
         System.out.println("Saldo disponível: ");
         double saldo = scanner.nextDouble();
 
+        System.out.println("Olá " + nomeCliente +
+                ", obrigado por criar uma conta em nosso banco. Sua agência é " +
+                agencia + ", conta " + numero +
+                " e seu saldo " + saldo + " já está disponível para saque!");
 
-        System.out.println("Olá" + nomeCliente +
-                ", obrigado por criar uma conta em nosso banco, sua agência é" +
-                 agencia + ", conta" + numero +
-                "e seu saldo " + saldo + "já disponível para saque!");
-
-
-
-
-
+        scanner.close();
     }
 }
